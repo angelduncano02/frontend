ac9e5254f1339563504309427c23cd46058fe633
angelduncano02 <angel.duncano@carsu.edu.ph>
2023-05-24 15:03:09 +0800

    Added toastify alerts

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -5,15 +5,29 @@ if (form) {
     e.preventDefault();
 
     const formData = new FormData(form);
-    const sentence = formData.get("sentence");
+    let sentence = formData.get("sentence");
 
-    try {
-      const response = await window.axios.openAI(sentence);
-
-      document.getElementById("sentence_corrected").innerHTML = JSON.stringify(response.choices[0].text).replace(/\\n/g, '');
-    } catch (error) {
-      console.error("Error:", error);
-      // Handle or display the error message appropriately
+    if (sentence.length <= 8) {
+      alertMessage("error", "Please input at least 8 characters!");
+      return;
     }
+
+    const response = await window.axios.openAI(formData.get("sentence"));
+    document.getElementById("sentence_corrected").innerHTML = JSON.stringify(response.choices[0].text).replace(/\\n/g, '');
   };
+}
+
+function alertMessage(status, sentence){
+  window.Toastify.showToast({
+    text: sentence,
+    duration: 5000,
+    stopOnFocus: true,
+    style: {
+      textAlign: "center",
+      background: status == "error" ? "red":"green",
+      color: "white",
+      padding: "5px",
+      marginTop: "2px"
+    }
+  });
 }
